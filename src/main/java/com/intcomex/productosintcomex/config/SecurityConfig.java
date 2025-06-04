87f12c9ead4f84680b37f69ba30543cb2edd843f
Julian Atencio <juat699@gmail.com>
2025-06-04 00:41:50 -0500

    Se adiciona swagger para documentación de endpoints

--- src/main/java/com/intcomex/productosintcomex/config/SecurityConfig.java
+++ src/main/java/com/intcomex/productosintcomex/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
